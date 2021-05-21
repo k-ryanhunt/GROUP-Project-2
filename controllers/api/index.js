5ab9840d937461ebd5976cd1a55e592f5d562714
Captain Ahab <captainahab337@gmail.com>
2021-05-20 21:15:50 -0600

    new changes

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes.js');
+const projectRoutes = require('./projectRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
